5654bbe5596bbf0a37741adc9d0c7e1989ccf8ab
Dominic Becker <drb@uber.com>
2016-05-31 15:48:20 -0700

    solution skeleton

---
+++ flatten.py
@@ -0,0 +1,13 @@
+from node import node
+from util import *
+
+## feel free to write other helper functions
+
+
+# Flatten the right-down list into just a right list (i.e. a linked list)
+def flatten(head):
+    pass  # TODO write some code here
+
+if __name__ == '__main__':
+    # This will verify it works...
+    test(flatten)
